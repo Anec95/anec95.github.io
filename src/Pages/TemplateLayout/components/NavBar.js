eaabb06a7332349b2944b08310b449884f0bd7e1
Anec95 <miori.thomas95@gmail.com>
2023-03-31 18:40:30 +0200

    commit navigate

--- src/Pages/TemplateLayout/components/NavBar.js
+++ src/Pages/TemplateLayout/components/NavBar.js
@@ -1,16 +1,16 @@
 import { TemplateNav } from "../styledComponents/styledComponents"
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from "react-router-dom"
 import Arrow from "./Arrow"
 import MenuTemplateBtn from "./MenuTemplateBtn"
 import { useEffect } from "react"
 
 function NavBar(props) {
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
     useEffect(() => {
-        if(props.index === 0) {
-            navigate("/template/escape_room")
-        } 
+        // if(props.index === 0) {
+        //     navigate("/template/escape_room")
+        // } 
         // else if(props.index === 1) {
         //     navigate("/template/girl")
         // } else {

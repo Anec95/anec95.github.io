7dc94d33d57b88aff7d3fe2e999ac5af6bb58445
Anec95 <miori.thomas95@gmail.com>
2023-05-26 12:58:48 +0200

    new img

--- src/Data/dataCardTemplates.js
+++ src/Data/dataCardTemplates.js
@@ -1,6 +1,6 @@
 import escape_room from 'Assets/images/photo-template/escape_room.png'
 import soap_template from 'Assets/images/photo-template/soap_template.png'
-import tattoo_template from 'Assets/images/photo-template/tattoo_shop/pexels-brett-sayles-2192237.jpg'
+import tattoo_template from 'Assets/images/photo-template/tattoo_template.png'
 
 export const dataCardTemplates = [
     {
